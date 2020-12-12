5eb142eb121bd23d905a83021400477dd7c0478f
Cédric Simard <simardced31@gmail.com>
2020-12-12 22:31:40 +0100

    add secondary axis on the plot

--- DataProcessing.py
+++ DataProcessing.py
@@ -7,7 +7,9 @@ import numpy as np
 import datetime
 import os
 
+# for testing on windows : switch commented lines
 os.chdir('/home/pi/Scripts/python/')
+# os.chdir('Z:\Scripts\python')
 
 files = glob.glob(r"temperatures/*/*/*.csv", recursive=True)
 li = []
@@ -26,16 +28,40 @@ for filename in files:
 
 
 dataframe = pd.concat(li, axis='index').sort_index()
-dataframe = dataframe.interpolate()
-dataframe["Temperature_int"] = dataframe["Temperature_int"].rolling('10min').mean()
 
-# dataframe.plot()
-# plt.show()
+# Save last measured temperatures
+indoor_temp = int(dataframe.Temperature_int[dataframe.Temperature_int.last_valid_index()]*100)/100
+outdoor_temp = int(dataframe.Temperature_ext[dataframe.Temperature_ext.last_valid_index()]*100)/100
+
+# Interpolate missing samples and smoothing
+dataframe = dataframe.interpolate()
+dataframe["Temperature_int"] = dataframe["Temperature_int"].rolling('20min').mean()
+dataframe["Temperature_ext"] = dataframe["Temperature_ext"].rolling('30min').mean()
+
+# Save all computed data to a singular file
 dataframe.to_csv("complete_data.csv")
+
+# Plot data for the last 24 hours
 df_svg = dataframe[datetime.datetime.now() - pd.Timedelta('1 day'):]
-df_svg.plot()
+
+fig, ax1 = plt.subplots()
+# Indoor temperature
+df_svg["Temperature_int"].plot(color='tab:red')
+ax1.tick_params(axis='y', labelcolor='tab:red')
+ax1.set_ylabel('Température intérieure', color='tab:red')
 plt.grid(True, which="both", axis="y")
+# Outdoor temperature
+ax2 = ax1.twinx()
+df_svg["Temperature_ext"].plot(color='tab:blue')
+ax2.tick_params(axis='y', labelcolor='tab:blue')
+ax2.set_ylabel('Température extérieure', color='tab:blue')
+fig.tight_layout()
+
+# for testing on windows : switch commented lines
+# plt.show()
 plt.savefig("/var/www/html/last24h.svg")
-print("Température intérieure :", int(df_svg.Temperature_int[df_svg.Temperature_int.last_valid_index()]*100)/100, "°C")
+
+
+print("Température intérieure :",indoor_temp , "°C")
 print('<br>')
-print("Température extérieure :", int(df_svg.Temperature_ext[df_svg.Temperature_ext.last_valid_index()]*100)/100, "°C")
+print("Température extérieure :", outdoor_temp, "°C")
